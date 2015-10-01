0960710df4a3752a24d531f9145646f24c5a7118
Igor Ralic <iralic@outlook.com>
2015-10-02 00:34:36 +0200

    Initial project(s) commit.

---
+++ BehaviorsSDK_Bug/BehaviorsSDK_Bug.Shared/SimpleModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BehaviorsSDK_Bug
+{
+    public class SimpleModel
+    {
+        public bool IsLoading { get; set; } = false;
+    }
+}
---
+++ BehaviorsSDK_Bug/BehaviorsSDK_Bug.Shared/SimpleModelCollection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BehaviorsSDK_Bug
+{
+    public class SimpleModelCollection : List<SimpleModel>
+    {
+        public SimpleModelCollection()
+        {
+            this.Add(new SimpleModel());
+            this.Add(new SimpleModel());
+            this.Add(new SimpleModel());
+            this.Add(new SimpleModel());
+            this.Add(new SimpleModel());
+            this.Add(new SimpleModel());
+        }
+    }
+}
